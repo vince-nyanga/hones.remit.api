ba5e6cb7e7e8fbacbe9faede41e9f0205cdac024
Vincent Nyanga <vince.nyanga@hotmail.com>
2024-08-20 22:46:59 +0200

    add saga state

---
+++ src/Hones.Remit.Api/MassTransit/Sagas/StateMachine/OrderState.cs
@@ -0,0 +1,26 @@
+using MassTransit;
+
+namespace Hones.Remit.Api.MassTransit.Sagas.StateMachine;
+
+public class OrderState : SagaStateMachineInstance
+{
+    public Guid CorrelationId { get; set; }
+    public Guid OrderId { get; set; }
+    public required string CurrentState { get; set; }
+    public DateTimeOffset? DateCreatedUtc { get; set; }
+    public DateTimeOffset? DatePaidUtc { get; set; }
+    public DateTimeOffset? DateCancelledUtc { get; set; }
+    public DateTimeOffset? DateReadyForCollection { get; set; }
+    public DateTimeOffset? DateCollectedUtc { get; set; }
+    public DateTimeOffset? DateExpiredUtc { get; set; }
+
+    internal static class States
+    {
+        public const string Created = "Created";
+        public const string Paid = "Paid";
+        public const string Cancelled = "Cancelled";
+        public const string ReadyForCollection = "ReadyForCollection";
+        public const string Collected = "Collected";
+        public const string Expired = "Expired";
+    }
+}
---
+++ src/Hones.Remit.Api/MassTransit/Sagas/StateMachine/OrderStateMachine.cs
@@ -0,0 +1,8 @@
+using MassTransit;
+
+namespace Hones.Remit.Api.MassTransit.Sagas.StateMachine;
+
+public class OrderStateMachine : MassTransitStateMachine<OrderState>
+{
+    
+}
